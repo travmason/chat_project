49697ef6ca64ffdd7774d4d8b35033f8e61f766f
Trav Mason <travis.mason@gmail.com>
2024-10-24 22:11:42 +1100

    logfile location fix

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -91,7 +91,7 @@ if 'RDS_DB_NAME' in os.environ:
             'file': {
                 'level': 'WARN',
                 'class': 'logging.FileHandler',
-                'filename': '/var/app/current/logs/deployment.log',
+                'filename': '/var/log/rolegenie/deployment.log',
             },
             'console': {
                 'level': 'WARN',
