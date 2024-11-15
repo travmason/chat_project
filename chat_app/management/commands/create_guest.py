8e02e88d7488adb3caa85868b59f7e83bc3379e7
Trav Mason <travis.mason@gmail.com>
2024-11-15 22:49:27 +1100

    static files cleaned up.

--- chat_app/management/commands/create_guest.py
+++ chat_app/management/commands/create_guest.py
@@ -1,3 +1,5 @@
+# UNUSED
+
 import logging
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission, ContentType
--- chat_project/settings.py
+++ chat_project/settings.py
@@ -198,7 +198,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "chat_app/static"]
+# STATICFILES_DIRS = [BASE_DIR / "chat_app/static"]
 STATIC_ROOT = BASE_DIR / "chat_app/staticfiles"
 STORAGES = {
     "default": {
