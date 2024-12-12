4f0ce38d88a2569d640464ab4d28be1f7aab0ce1
Trav Mason <travis.mason@gmail.com>
2024-12-13 00:57:22 +1100

    Update forms.py

--- chat_app/forms.py
+++ chat_app/forms.py
@@ -13,6 +13,7 @@ class CustomSignupForm(SignupForm):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.layout = Layout(
+            Field('Name', css_class='form-control', wrapper_class='form-group', placeholder='Enter your name'),
             Field('email'),
             Field('password1', css_class='form-control', wrapper_class='form-group', placeholder='Enter your password'),
             Field('password2', css_class='form-control', wrapper_class='form-group', placeholder='Confirm your password'),
