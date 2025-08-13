5a3e7443c1e63b086654ed545e5a5ed4e7ad608e
Trav Mason <travis.mason@gmail.com>
2025-08-13 22:56:27 +1000

    updated elasticache settings

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -314,7 +314,7 @@ CHANNEL_LAYERS = {
 # Broker and result backend can be overridden via environment variables so that
 # a remote message broker (e.g., Redis, SQS) can be shared between the web
 # application and separate Celery workers.
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://rolegenie-valcache-zpkaa5.serverless.apse2.cache.amazonaws.com:6379/0')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', CELERY_BROKER_URL)
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
